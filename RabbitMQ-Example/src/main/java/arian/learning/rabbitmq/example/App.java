68ce6d443c0a9db18c46f0be812d6de275800707
ArianZhang <arian_zhang@foxmail.com>
2013-11-13 17:49:45 +0800

    initial

---
+++ RabbitMQ-Example/src/main/java/arian/learning/rabbitmq/example/App.java
@@ -0,0 +1,13 @@
+package arian.learning.rabbitmq.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
